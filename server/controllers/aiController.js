f54a32fe27dea37728de098aa1fdfdd22c26d252
Patricia Aguiar <paguiar.eng@gmail.com>
2025-12-11 22:35:06 -0300

    back-end: integrated Gemini functionality to reviewResume controller

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -4,6 +4,7 @@ import { clerkClient } from "@clerk/express";
 import axios from "axios";
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+import pdf from "pdf-parse/lib/pdf-parse.js";
 
 const AI = new OpenAI({
   apiKey: process.env.GEMINI_API_KEY,
@@ -210,7 +211,7 @@ export const removeImageObject = async (req, res) => {
 export const resumeReview = async (req, res) => {
   try {
     const { userId } = req.auth();
-    const { resume } = req.file;
+    const resume = req.file;
     const plan = req.plan;
 
     if (plan !== "premium") {
@@ -228,10 +229,27 @@ export const resumeReview = async (req, res) => {
     }
 
     const dataBuffer = fs.readFileSync(resume.path);
+    const pdfData = await pdf(dataBuffer);
 
-    await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, ${`Removed ${object} from image`}, ${imageUrl}, 'image')`;
+    const prompt = `Review the following resume and provide constructive feedback on its strengths, weaknesses, and areas for improvement. Resume Content: \n\n ${pdfData.text}`;
 
-    res.json({ success: true, content: imageUrl });
+    const response = await AI.chat.completions.create({
+      model: "gemini-2.0-flash",
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      temperature: 0.7,
+      max_tokens: 1000,
+    });
+
+    const content = response.choices[0].message.content;
+
+    await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId}, 'Review the uploaded resume, ${content}, 'resume-review')`;
+
+    res.json({ success: true, content });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
