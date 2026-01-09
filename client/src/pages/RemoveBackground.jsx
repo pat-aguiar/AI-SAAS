a8b16962ba6fe35c5580e5ba46244bfbcdfb42ed
Patricia Aguiar <paguiar.eng@gmail.com>
2026-01-09 00:02:25 -0300

    debugged removeBackground frontend

--- client/src/pages/RemoveBackground.jsx
+++ client/src/pages/RemoveBackground.jsx
@@ -1,6 +1,7 @@
 import { Eraser, Sparkles } from "lucide-react";
 import React, { useState } from "react";
 import axios from "axios";
+import { useAuth } from "@clerk/clerk-react";
 import toast from "react-hot-toast";
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
